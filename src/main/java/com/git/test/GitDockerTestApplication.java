f168295e824456a47781f6632c5fa069db8a1ea9
dhaneshvl <dhaneshwaranvl@gmail.com>
2023-11-15 21:50:28 +0300

    modified the cicd workflow yml file

--- src/main/java/com/git/test/GitDockerTestApplication.java
+++ src/main/java/com/git/test/GitDockerTestApplication.java
@@ -17,13 +17,4 @@ public class GitDockerTestApplication {
     public static void main(String[] args) {
         SpringApplication.run(GitDockerTestApplication.class, args);
     }
-
-//	echo "# git-test-app" >> README.md
-//	git init
-//	git add README.md
-//	git commit -m "first commit"
-//	git branch -M main
-//	git remote add origin git@github.com:dhaneshvl/git-test-app.git
-//	git push -u origin main
-
 }
